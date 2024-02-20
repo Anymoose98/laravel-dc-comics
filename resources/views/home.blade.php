b35365b360d59178daa1b4dc801741fe0b46cb67
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-02-20 19:07:19 +0100

    Creato il controller

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
             <div class="container pt-5">
                 <div class="row">
                     {{-- Contenuto  --}}
-                    {{-- @foreach ($db as $db)
+                    {{-- @foreach ($comics as $comic)
                     <div class="col-2 my-4">
                         <a href="{{ route('dettaglio_fumetto', ['param' => $db['id']]) }}">
                             <div class="text-center effetto-hover">
